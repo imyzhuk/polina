ea76c5e9ae1f24a50f9d644eec156e94ee619055
Maksim Zhuk <m>
2022-09-01 20:56:18 +0300

    feat

--- js/main.js
+++ js/main.js
@@ -7,26 +7,26 @@ window.onload = () => {
         return image;
     }
 
+    const options = Object.freeze({
+        token: '5751275639:AAGluIWl2c6VlVGrM-7hCecKkJjs02gcDKM',
+        chatId: -795909208
+    })
+    
+    const createSendMessageUrl = (message, options) => `https://api.telegram.org/bot${options.token}/sendMessage?chat_id=${options.chatId}&text=${message}&parse_mode=HTML`;
+    const createMessageText = (name, phone) => `<b>Имя</b>: ${name}; <b>Номер</b>: ${phone}`;
+
     form.addEventListener('submit', e => {
         e.preventDefault();
 
         const formData = Object.fromEntries(new FormData(e.target));
-
-        console.log({
-            name: formData.name,
-            phone: formData.phone
-        });
-        fetch("http://localhost:3000", {
-            method: 'POST',
-            body: JSON.stringify(formData),
-            // headers: {
-            //     'Content-Type': 'application/json'
-            // }
+        fetch(createSendMessageUrl(createMessageText(formData.name, formData.phone), options), {
+            method: 'POST'
         }).then(() => {
-            const image = createImage('assets/images/checkmark.svg', "signup__btn-image", 'Отправлено');
-            btn.append(image);
-            btn.innerText = "Отправлено";
-        })
+            const btnText  = document.querySelector('.signup__btn-text');
+            const btnImage  = document.querySelector('.signup__btn-image');
+            btnText.innerText = "Отправлено";
+            btnImage.classList.add('signup__btn-image_visible')
+        }).catch(error => error)
     })
 
     // const headerHint = document.querySelector('.header__hint');
